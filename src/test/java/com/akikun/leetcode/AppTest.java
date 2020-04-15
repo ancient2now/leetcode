b7a5ae1db9b67663527e67bef35ab2a6b24b0a26
Li Junqiu <ancient2now@163.com>
2020-04-15 22:24:03 +0800

    add revertWords

--- src/test/java/com/akikun/leetcode/AppTest.java
+++ src/test/java/com/akikun/leetcode/AppTest.java
@@ -38,4 +38,95 @@ public class AppTest
         return x % 10 + x / 10 + y % 10 + y / 10;
     }
 
+    @Test
+    public void reverseWords() {
+        String s = "  8   ";
+        System.out.print(reverseWords(s));
+        System.out.println("@");
+
+    }
+
+    private String reverseWords(String s) {
+        String[] arr = s.split("\\s+");
+        if(arr.length == 0 || s.isEmpty()) return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = arr.length - 1; i > -1; --i) {
+            if (!arr[i].isEmpty()) {
+                sb.append(arr[i]).append(" ");
+            }
+        }
+        return sb.substring(0, sb.length() - 1);
+    }
+
+    @Test
+    public void gameOfLife() {
+
+        int[][] board = new int[][]{{0,1,0},{0,0,1},{1,1,1},{0,0,0}};
+
+        gameOfLife(board);
+
+        printMatrix(board);
+
+
+    }
+
+    public void gameOfLife(int[][] board) {
+
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; ++i)  {
+            for (int j = 0; j < n; ++j) {
+                int cnt = count(i, j, m, n, board);
+                if ((board[i][j] & 1) == 1) {
+                    if (cnt == 2 || cnt == 3) {
+                        board[i][j] = 0b11;
+                    }
+                } else {
+                    if (cnt == 3) {
+                        board[i][j] = 0b10;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < m; ++i) {
+            for (int j =0; j < n; ++j) {
+                board[i][j] >>= 1;
+            }
+        }
+
+    }
+
+    private int count(int i, int j, int m, int n, int[][] board) {
+        int cnt = (board[i][j] & 1) == 1 ? -1 : 0;
+        for (int dx = -1; dx < 2; ++dx) {
+            for (int dy = -1; dy < 2; ++dy) {
+                int x = i + dx;
+                int y = j + dy;
+                if (x < 0 || x >= m || y < 0 || y >= n) {
+                    continue;
+                }
+                if ((board[x][y] & 1) == 1) {
+                    cnt ++;
+                }
+            }
+        }
+        return cnt;
+    }
+
+    public void printMatrix(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                sb.append(board[i][j]).append(',');
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+
 }
