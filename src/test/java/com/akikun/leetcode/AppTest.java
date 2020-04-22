ba3b014f6cc2bb57c761520ae7f13b5e6cb1e2a3
Li Junqiu <ancient2now@163.com>
2020-04-22 21:45:00 +0800

    update AppTest

--- src/test/java/com/akikun/leetcode/AppTest.java
+++ src/test/java/com/akikun/leetcode/AppTest.java
@@ -1,6 +1,18 @@
 package com.akikun.leetcode;
 
+import jdk.nashorn.internal.ir.WhileNode;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Unit test for simple App.
@@ -128,5 +140,79 @@ public class AppTest
         }
         System.out.print(sb.toString());
     }
+
+
+    int[] dx = new int[]{0, 0, 1, -1};
+    int[] dy = new int[]{-1, 1, 0, 0};
+
+    public int[][] updateMatrix(int[][] matrix) {
+        int w = matrix.length;
+        int h = matrix[0].length;
+
+        List<int[]> list = new LinkedList<>();
+
+        for (int i = 0; i < w; ++i) {
+            for (int j = 0; j < h; ++j) {
+                if (matrix[i][j] == 1) {
+                    if (!haveNumber(i, j, w, h, matrix, 0)) {
+                        matrix[i][j] = -1;
+                        list.add(new int[]{i, j});
+                    }
+                }
+            }
+        }
+
+        int distance = 1;
+        int[] coard;
+        while (!list.isEmpty()) {
+            distance ++;
+            for (int i = list.size() - 1; i >= 0; --i) {
+                coard = list.get(i);
+                if (haveNumber(coard[0], coard[1], w, h, matrix,distance - 1)) {
+                    matrix[coard[0]][coard[1]] = distance;
+                    list.remove(i);
+                }
+            }
+        }
+
+        return matrix;
+
+    }
+
+    private boolean haveNumber(int i, int j, int w, int h, int[][] matrix, int target) {
+        for (int d = 0; d < 4; ++d) {
+            int x = i + dx[d], y = j + dy[d];
+            if (x < 0 || y < 0 || x >= w || y >= h) {
+                continue;
+            }
+            if (matrix[x][y] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int[][] merge(int[][] arr) {
+        int len = arr.length;
+        if (len <= 1) {
+            return arr;
+        }
+
+        List<int[]> list = Arrays.stream(arr)
+                .sorted(Comparator.comparingInt(a -> a[0]))
+                .collect(Collectors.toList());
+        for (int i = list.size() - 1; i > 0; --i) {
+            int[] after = list.get(i);
+            int[] before = list.get(i - 1);
+            if (after[0] < before[1]) {
+                before[1] = Math.max(before[1], after[1]);
+                list.remove(i);
+            }
+        }
+//        Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
+        return list.toArray(new int[list.size()][2]);
+    }
+
+
 
 }
