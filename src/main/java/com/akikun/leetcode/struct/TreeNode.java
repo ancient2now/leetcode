1773d92e3e52f10b36c94ce1aece90b9250592bd
Li Junqiu <ancient2now@163.com>
2020-07-21 20:55:19 +0800

    add TreeNode constructor

--- src/main/java/com/akikun/leetcode/struct/TreeNode.java
+++ src/main/java/com/akikun/leetcode/struct/TreeNode.java
@@ -5,7 +5,17 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
+    public TreeNode() {
+
+    }
+
     public TreeNode(int x) {
         val = x;
+    }
+
+    public TreeNode(int x, TreeNode left, TreeNode right) {
+        val = x;
+        this.left = left;
+        this.right = right;
     }
 }
